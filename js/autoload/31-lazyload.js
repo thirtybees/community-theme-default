a78855f89f65f5d2d824eb0356484aa0debd15f6
Michael Dekker <git@michaeldekker.nl>
2018-01-20 17:20:27 +0100

    Firefox lazy loading / picture fixes

--- js/autoload/31-lazyload.js
+++ js/autoload/31-lazyload.js
@@ -26,9 +26,15 @@
 
     /**
      * Preloads the image
-     * @param {object} image
+     * @param {object} elem
      */
-    function preloadImage(image) {
+    function preloadImage(elem) {
+      // If this IMG or SOURCE is part of a PICTURE, make sure to check all childNodes at once
+      var image = elem;
+      if (elem.parentNode.tagName === 'PICTURE') {
+        image = elem.parentNode;
+      }
+
       if (image.tagName === 'PICTURE') {
         // This picture contains several SOURCEs and IMGs
         [].slice.call(image.childNodes).forEach(function (childNode) {
@@ -88,14 +94,13 @@
       }
     }
 
-    var observer;
+    var observer = lozad('.tb-lazy-image, picture.tb-lazy-image img, picture.tb-lazy-image source', {
+      load: function (el) {
+        preloadImage(el);
+      }
+    });
+
     setInterval(function () {
-      observer = null;
-      observer = lozad('.tb-lazy-image', {
-        load: function (el) {
-          preloadImage(el);
-        }
-      });
       observer.observe();
     }, 500);
   }
